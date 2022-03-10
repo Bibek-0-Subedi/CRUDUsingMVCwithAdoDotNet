daff8b34375172062d24f7e7ad9b5c05d9448010
bibeksubedi17 <bibeksubedi17@gmail.com>
2022-03-10 17:28:32 +0545

    Add project files.

---
+++ Models/EmpModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CRUDUsingMVCwithAdoDotNet.Models
+{
+    public class EmpModel
+    {
+        [Key]
+        [Display(Name = "ID")]
+        public int Empid { get; set; }
+
+        [Required(ErrorMessage = "Name field is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "City field is required")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Address field is required")]
+        public string Address { get; set; }
+    }
+}
